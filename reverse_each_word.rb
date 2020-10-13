ac382659eb21a00276b5b442f3c7c1daa5c05bac
jmsimmons90 <kutsuukun@gmail.com>
2020-10-12 18:50:39 -0700

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 def reverse_each_word(sentence)
     new_array = sentence.split(" ")
-    new_array.collect {|element|  "#{element.reverse}"}
+    second_array = new_array.collect {|element| "#{element.reverse}" }
+    return "#{second_array.join(" ")}"
 end
